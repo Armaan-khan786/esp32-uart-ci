2b2c44a6a918c3cc89938b93359708733688cd41
Armaan-khan786 <a75777129@gmail.com>
2026-02-05 18:45:49 +0530

    Refactor UART logging in esp32_flash.bat

--- esp32_flash.bat
+++ esp32_flash.bat
@@ -6,14 +6,9 @@ set RESULT=FAIL
 set UART_LOG=%TEMP%\uart_result.txt
 del "%UART_LOG%" 2>nul
 
-for /L %%i in (1,1,15) do (
+for /L %%i in (1,1,20) do (
 
-    powershell -NoProfile -Command ^
-      "$p = New-Object System.IO.Ports.SerialPort('%RECEIVER_PORT%',%BAUD%); ^
-       $p.Open(); ^
-       Start-Sleep -Milliseconds 500; ^
-       if ($p.BytesToRead -gt 0) { $p.ReadLine() | Out-File -Append '%UART_LOG%' }; ^
-       $p.Close();"
+    powershell -NoProfile -Command "$p=New-Object System.IO.Ports.SerialPort('%RECEIVER_PORT%',%BAUD%); $p.ReadTimeout=500; $p.Open(); Start-Sleep -Milliseconds 400; if($p.BytesToRead -gt 0){ $p.ReadExisting() | Out-File -Append '%UART_LOG%' }; $p.Close()"
 
     if exist "%UART_LOG%" (
         findstr /C:"UART_TEST_PASS" "%UART_LOG%" >nul && (
